30c76d8e86e114d33117e368933f644a32573126
Dion Almaer <dion@almaer.com>
2023-02-15 21:53:41 -0700

    CORS WORKZ?!?

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ import polymath
 from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template, request
 from flask_compress import Compress
+from flask_cors import CORS
+
 from google.cloud import firestore
 from polymath.library import vector_from_base64
 
 DEFAULT_TOKEN_COUNT = 1000
 
 app = Flask(__name__)
+CORS(app)
 Compress(app)
 
 load_dotenv()
