2ebd75ee7de7faab47ce00bfe44af39c21ccfede
Dimitri Glazkov <dimitri@glazkov.com>
2023-02-26 21:46:41 -0800

    Convert to use the new goodness. Fixes #5, #3, and #1.

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import argparse
-import os
 import traceback
 from urllib.parse import urlparse
 
-import numpy as np
-import pinecone
-import polymath
 from dotenv import load_dotenv
-
 from flask import Flask, jsonify, render_template, request
 from flask_compress import Compress
 from flask_cors import CORS
-
 from google.cloud import firestore
-from polymath.library import vector_from_base64
+from polymath.pinecone import PineconeLibrary
 
 DEFAULT_TOKEN_COUNT = 1000
 
@@ -38,60 +32,35 @@ class ConfigStore:
 store = ConfigStore()
 
 
+def make_args(request):
+    content_type = request.headers.get('Content-Type')
+    args = {}
+    if (content_type == 'application/json'):
+        json = request.json
+        if not json:
+            return jsonify({
+                "error": "No arguments provided"
+            })
+        args = {
+            'count': DEFAULT_TOKEN_COUNT,
+            **json
+        }
+    else:
+        args = {
+            'count': DEFAULT_TOKEN_COUNT,
+            **request.form.to_dict()
+        }
+    return args
+
+
 @app.route("/", methods=["POST"])
 def start():
     try:
-        library = polymath.Library()
         config = store.get(request.base_url)
-        if library.embedding_model != request.form.get("query_embedding_model"):
-            raise Exception("Library embedding model mismatch")
-        if library.version != request.form.get('version', -1, type=int):
-            raise Exception("Library version mismatch")
-        query_embedding = request.form.get("query_embedding")
-        count = request.form.get(
-            "count", DEFAULT_TOKEN_COUNT, type=int)
-        count_type = request.form.get("count_type")
-        sort = request.form.get('sort')
-        sort_reversed = request.form.get('sort_reversed') is not None
-        seed = request.form.get('seed')
-        omit = request.form.get('omit')
-        access_token = request.form.get('access_token', '')
-        library.omit = 'embedding'
-        pinecone.init(
-            api_key=os.getenv("PINECONE_API_KEY"),
-            environment=PINECONE_ENVIRONMENT)
-        index = pinecone.Index('polymath')
-        embedding = None
-        if sort == 'similarity':
-            embedding = vector_from_base64(query_embedding).tolist()
-        else:
-            embedding = np.random.rand(1536).tolist()
-        result = index.query(
-            namespace=config['pinecone']['namespace'],
-            top_k=10,
-            include_metadata=True,
-            vector=embedding
-        )
-        token_count = 0
-        for item in result['matches']:
-            token_count += item['metadata'].get('token_count')
-            if token_count > DEFAULT_TOKEN_COUNT:
-                print(f'Breaking at {token_count} tokens')
-                break
-            bit = polymath.Bit(data={
-                'id': item['id'],
-                'text': item['metadata']['text'],
-                'token_count': item['metadata'].get('token_count'),
-                'access_tag': item['metadata'].get('access_tag'),
-                'info': {
-                    'url': item['metadata']['url'],
-                    'image_url': item['metadata'].get('image_url'),
-                    'title': item['metadata'].get('title'),
-                    'description': item['metadata'].get('description'),
-                }
-            })
-            library.insert_bit(bit)
-        return jsonify(library.serializable())
+        library = PineconeLibrary(config["pinecone"])
+        args = make_args(request)
+        result = library.query(args)
+        return jsonify(result.serializable())
 
     except Exception as e:
         return jsonify({
